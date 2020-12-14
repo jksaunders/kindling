5cb90ddd08d893dd86f3d2e0850d09e6d86bab37
Joshua Saunders <jk.saunders@hotmail.com>
2020-12-14 17:13:17 -0500

    Added snapshot testing

--- src/components/pages/ExamplePage/ExamplePage.spec.tsx
+++ src/components/pages/ExamplePage/ExamplePage.spec.tsx
@@ -3,7 +3,10 @@ import { render, screen } from '@testing-library/react';
 import ExamplePage from './ExamplePage';
 
 test('renders', async () => {
-  render(<ExamplePage />);
+  const {
+    container: { firstChild },
+  } = render(<ExamplePage />);
   await screen.findByText("I'm an example page!");
   expect(screen.getByText("I'm an example page!")).toBeInTheDocument();
+  expect(firstChild).toMatchSnapshot();
 });
