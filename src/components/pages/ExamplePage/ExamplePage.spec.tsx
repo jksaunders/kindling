7fb5a2b7b53833088b975337c2c9b51425abc84e
Joshua Saunders <jk.saunders@hotmail.com>
2020-12-14 22:28:48 -0500

    Fixed broken tests

--- src/components/pages/ExamplePage/ExamplePage.spec.tsx
+++ src/components/pages/ExamplePage/ExamplePage.spec.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ExamplePage from './ExamplePage';
+import { LocalStoresRoot } from '../../../stores';
 
 test('renders', async () => {
   const {
     container: { firstChild },
-  } = render(<ExamplePage />);
+  } = render(
+    <LocalStoresRoot>
+      <ExamplePage />
+    </LocalStoresRoot>
+  );
   await screen.findByText("I'm an example page!");
   expect(screen.getByText("I'm an example page!")).toBeInTheDocument();
   expect(screen.getByText("I'm an example page!")).toHaveStyle(
