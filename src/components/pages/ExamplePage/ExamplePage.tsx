53f5e1c15d8bcb19d14af6e2918880d9dc4cd3f0
Joshua Saunders <jk.saunders@hotmail.com>
2020-12-16 21:07:06 -0500

    Added react-spring + example (#7)
    * Added react-spring + example
    
    * Updated snapshot

--- src/components/pages/ExamplePage/ExamplePage.tsx
+++ src/components/pages/ExamplePage/ExamplePage.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite';
 import { FC } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { useTransition, animated } from 'react-spring';
 import useStores from '../../../stores';
 import { ROUTES } from '../../Routes';
 
@@ -9,6 +10,31 @@ const StyledComponentsExample = styled.div`
   border-radius: 10px;
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1);
 `;
+
+interface AnimatedButtonProps {
+  onClick: () => void;
+}
+
+const AnimatedButton: FC<AnimatedButtonProps> = ({ children, onClick }) => {
+  const transitions = useTransition(true, null, {
+    from: { transform: 'translate3d(0,-40px,0)' },
+    enter: { transform: 'translate3d(0,0px,0)' },
+    leave: { transform: 'translate3d(0,-40px,0)' },
+  });
+
+  return (
+    <>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.button key={key} style={props} onClick={onClick}>
+              {children}
+            </animated.button>
+          )
+      )}
+    </>
+  );
+};
 
 const StoresExampleUsage = observer(() => {
   const { userStore } = useStores();
@@ -21,9 +47,9 @@ const ExamplePage: FC = () => {
     <StyledComponentsExample>
       I&apos;m an example page!
       <StoresExampleUsage />
-      <button onClick={(): void => history.push(ROUTES.ANOTHER_PAGE)}>
+      <AnimatedButton onClick={(): void => history.push(ROUTES.ANOTHER_PAGE)}>
         Go to another page
-      </button>
+      </AnimatedButton>
     </StyledComponentsExample>
   );
 };
